e29a916753173c8fcb7bb762c632ed6658726fe9
aritzavila <131864025+aritzavila@users.noreply.github.com>
2023-05-30 20:03:03 +0200

    Update rama1.java

--- rama1.java
+++ rama1.java
@@ -1,8 +1,8 @@
 public class rama1 {
 
 	public static void main(String[] args) {		
-		System.out.println("Me gusta Leire");
-		System.out.println("Alvaro es gay");
+		System.out.println("Me gusta Julia");
+		System.out.println("Alvaro es muyyyyyyyyy gay");
 	}
 
 }
