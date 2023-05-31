3e24290ff910d06d942bda22738379605aea541c
Aritz Avila <aavilar04@educarex.es>
2023-05-31 07:05:06 +0000

    dfghuj

--- rama1.java
+++ rama1.java
@@ -1,9 +1,6 @@
 public class rama1 {
 
 	public static void main(String[] args) {		
-		System.out.println("Me gusta Leire");
-		System.out.println("Alvaro es gay");
-
 		int x=0;
 		int y=3;
 		System.out.println(x+y);
